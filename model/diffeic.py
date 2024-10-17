a925954f93086dbe1bfbb95cc5159803b1eae957
Sachit Munjal <96139801+sachitmunjal@users.noreply.github.com>
2024-10-17 16:49:36 +0530

    Update diffeic.py

--- model/diffeic.py
+++ model/diffeic.py
@@ -783,7 +783,7 @@ class DiffEIC(LatentDiffusion):
         # lpips_loss = lpips_loss_function(model_output[:, :3, :, :], x_noisy[:, :3, :, :])
         print(lpips_loss.shape)
         print(loss.shape)
-        lpips_loss = lpips_loss.view(4, 1, 1, 1) 
+        lpips_loss = lpips_loss.unsqueeze(0).expand(4, 1, 1, 1)  # Expand the scalar to match the shape of loss
         loss += lpips_loss
         
         loss_bpp = cond['bpp']
